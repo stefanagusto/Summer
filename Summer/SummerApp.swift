b058b4f1bf42c2a7d187104f37c8c46d1fc647af
Stefan Agusto Hutapea <stefanhutapea@gmail.com>
2024-05-20 09:28:45 +0700

    Initial Commit

---
+++ Summer/SummerApp.swift
@@ -0,0 +1,17 @@
+//
+//  SummerApp.swift
+//  Summer
+//
+//  Created by Stefan Agusto Hutapea on 20/05/24.
+//
+
+import SwiftUI
+
+@main
+struct SummerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
